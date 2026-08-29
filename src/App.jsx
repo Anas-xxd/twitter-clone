61cbf3e0694bf072cb00d21473bc37e9672fe168
Anas barodi <anas.xx200@gmail.com>
2026-08-29 10:54:38 +0300

    style: add initial style to homepage with css

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import logo from "./assets/logo.svg";
+import avatar from "./assets/avatar.svg";
+import postImg from "./assets/content-placeholder.png";
 import {
   Menu,
   Heart,
@@ -12,85 +15,100 @@ export default function App() {
   return (
     <>
       <header>
-        <button className="menu-btn">
+        <button className="btn menu-btn">
           <Menu />
         </button>
 
         <div className="logo-container">
-          <svg id="logo"></svg>
+          <img src={logo} id="logo" alt="Logo" />
         </div>
 
-        <button className="account-btn">
-          <img className="avatar" src="" alt="" />
+        <button className="btn account-btn">
+          <img className="avatar" src={avatar} alt="Avatar" />
         </button>
       </header>
 
-      <div className="feed-type-btn" role="tablist">
-        <button className="for-you-btn" role="tab" aria-selected="true">
-          For you
-        </button>
-        <button className="following-btn" role="tab" aria-selected="false">
-          Following
-        </button>
-      </div>
-
-      <form className="post-section">
-        <img className="avatar" src="" alt="" />
-
-        <div className="text-area">
-          <textarea placeholder="What's happening?"></textarea>
-          <button type="submit">Post</button>
+      <main>
+        <div className="feed-type-tablist" role="tablist">
+          <button className="btn tab-btn" role="tab" aria-selected="true">
+            For you
+          </button>
+          <button className="btn tab-btn" role="tab" aria-selected="false">
+            Following
+          </button>
         </div>
-      </form>
 
-      <section className="feed-section">
-        <article className="post">
-          <img className="avatar" src="" alt="" />
+        <form className="post-section">
+          <img className="avatar" src={avatar} alt="Avatar" />
 
-          <div className="post-content">
-            <p className="user-info">Username @user date</p>
-
-            <p className="content"> today I am so lazy</p>
-
-            <div className="post-interactions">
-              <button className="interaction-btn" aria-label="Like">
-                <Heart />
-              </button>
-
-              <button className="interaction-btn" aria-label="Reply">
-                <MessageCircle />
-              </button>
-
-              <button className="interaction-btn" aria-label="Repost">
-                <Repeat2 />
-              </button>
-            </div>
+          <div className="post-text-area">
+            <textarea
+              className="text-area"
+              placeholder="What's happening?"
+            ></textarea>
+            <button className="post-btn" type="submit">
+              Post
+            </button>
           </div>
-        </article>
-      </section>
+        </form>
+
+        <section className="feed-section">
+          <article className="post">
+            <img className="avatar" src={avatar} alt="Avatar" />
+
+            <div className="post-content">
+              <p className="user-info">
+                Username <span>@user . Aug 26</span>
+              </p>
+
+              <p className="content"> today I am so lazy</p>
+
+              <img className="img-post" src={postImg} alt="Image Post" />
+
+              <div className="post-interactions">
+                <button className="btn interaction-btn" aria-label="Like">
+                  <Heart />
+                </button>
+
+                <button className="btn interaction-btn" aria-label="Reply">
+                  <MessageCircle />
+                </button>
+
+                <button className="btn interaction-btn" aria-label="Repost">
+                  <Repeat2 />
+                </button>
+              </div>
+            </div>
+          </article>
+        </section>
+      </main>
 
       <footer>
-        <nav aria-label="Primary">
+        <nav className="main-nav" aria-label="Primary">
           <a
             href=""
-            className="nav-btn"
+            className="btn nav-btn"
             aria-label="Home-page-button"
             aria-current="page"
           >
             <House />
           </a>
 
-          <a href="" className="nav-btn" aria-label="Search-page-button">
+          <a href="" className="btn nav-btn" aria-label="Search-page-button">
             <Search />
           </a>
 
-          <a href="" className="nav-btn" aria-label="notification-page-button">
+          <a
+            href=""
+            className="btn nav-btn"
+            aria-label="notification-page-button"
+          >
             <Bell />
           </a>
 
           <a
             href=""
-            className="nav-btn"
+            className="btn nav-btn"
             aria-label="direct-message-page-button"
           >
             <MessageCircle />
