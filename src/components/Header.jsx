ec4ec7f734072d52565ef2f5eb7527ef699f5c6b
Anas barodi <anas.xx200@gmail.com>
2026-09-10 11:35:39 +0000

    feat: add auto-hide header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,19 @@
 import logo from "../assets/logo.svg";
 import avatar from "../assets/avatar.svg";
 import FeedTypeTab from "./FeedTypeTabs";
+import { useHeaderVisibility } from "./useHeaderVisibility.js";
 import { Menu } from "lucide-react";
 
 export default function Header() {
+  const isHeaderVisible = useHeaderVisibility(20);
+
   function showNavBar() {
     document.querySelector(".nav-bar").classList.toggle("invisible");
     document.querySelector(".gray-background").classList.toggle("invisible");
   }
 
   return (
-    <header>
+    <header className={isHeaderVisible ? "visible" : "hidden"}>
       <div className="header-container">
         <button className="btn menu-btn" onClick={showNavBar}>
           <Menu />
